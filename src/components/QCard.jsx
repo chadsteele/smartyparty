6acf930d00403ad57c983d65195a60a9cc67cbe7
chadsteele <chad@chadsteele.com>
2024-04-27 04:07:20 +0400

    improved error message

--- src/components/QCard.jsx
+++ src/components/QCard.jsx
@@ -104,7 +104,8 @@ export default function (props) {
                 />
                 <Show when={hint() && answer() != props.qa.a} >
                     <Alert severity="error" sx={{ marginTop: '1em' }}>
-                        Hint: &nbsp;  <strong>{hint()}</strong>
+                        <Box>Sorry.  You will need to answer this question correctly 3 times now to ensure you've learned it.  Punctuation and capitalization matters.  The Hints improve everytime you hit enter.</Box>
+                        <Box> Hint: &nbsp;  <strong>{hint()}</strong></Box>
                     </Alert>
                 </Show>
 
